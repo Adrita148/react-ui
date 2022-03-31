8d48d64f5ea66715b9883255fcfb803080b2d955
Soumyadeep Mukherjee <soumyadeep@yosecal.com>
2022-03-31 19:09:48 +0530

    changed color of grid and made it fit and all

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,19 @@ import { columns, rows } from "./vars";
 
 export default function Header() {
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: "510px", width: "100%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
         checkboxSelection
         disableSelectionOnClick
-      />
+        sx={{
+          backgroundColor: "#2a3e4c",
+          border: "0px",
+          color: "white",
+          borderRadius: "0px",
+        }}
+      ></DataGrid>
     </div>
   );
 }
